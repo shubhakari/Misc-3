56931d8342705ceb14de5d16851c0690eed527f4
shubhakari <shubhakaridikshith@gmail.com>
2025-04-17 11:34:30 -0700

    Completed Misc-3

---
+++ shipWithinDays.py
@@ -0,0 +1,22 @@
+class Solution:
+    # Time complexity: O(N*log(S))
+    # SC : O(1)
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        maxWeight, totalWeight = -1, 0
+        for weight in weights:
+            maxWeight = max(maxWeight, weight)
+            totalWeight += weight
+        left, right = maxWeight, totalWeight
+        while left < right:
+            mid = (left + right) // 2
+            daysNeeded, currWeight = 1, 0
+            for weight in weights:
+                if currWeight + weight > mid:
+                    daysNeeded += 1
+                    currWeight = 0
+                currWeight += weight
+            if daysNeeded > days:
+                left = mid + 1
+            else:
+                right = mid
+        return left
